87b2443e58a24cedbd0d87f0e3eca6b2ca933208
jadon belezos <jadon.belezos@autodesk.com>
2016-07-01 10:30:26 -0700

    changed style of bracing again

--- src/I2C.cpp
+++ src/I2C.cpp
@@ -12,27 +12,23 @@ void i2CInitialize(uint8_t port, int32_t* status){}
 
 	int32_t i2CTransaction(uint8_t port, uint8_t deviceAddress, uint8_t* dataToSend,
 		uint8_t sendSize, uint8_t* dataReceived,
-		uint8_t receiveSize)
-	{
+		uint8_t receiveSize){
 		//call i2c read and i2c write for transactions.
 		return 0;
 	}
 	int32_t i2CWrite(uint8_t port, uint8_t deviceAddress, uint8_t* dataToSend,
-	 uint8_t sendSize)
-	{
+	 uint8_t sendSize){
 	// write are always successful because there is no where for the data to go
 		return 0;
 	}
 
 
 	int32_t i2CRead(uint8_t port, uint8_t deviceAddress, uint8_t* buffer,
-		uint8_t count)
-	{
+		uint8_t count){
 	// rewrite buffer with 0's
 	return 0;
 	}
-	void i2CClose(uint8_t port)	
-	{
+	void i2CClose(uint8_t port){
 		//not implemented because 
 		//we are not emulating i2c protocol
 	}
--- src/Power.cpp
+++ src/Power.cpp
@@ -1,60 +1,46 @@
 #include "HAL\Power.hpp"
 extern "C"
 {
-	float getVinVoltage(int32_t *status) 
-	{ 
+	float getVinVoltage(int32_t *status){ 
 		return 12.1f; 
 	}
-	float getVinCurrent(int32_t *status)
-	{ 
+	float getVinCurrent(int32_t *status){ 
 		return 0.0f; 
 	}
-	float getUserVoltage6V(int32_t *status)
-	{ 
+	float getUserVoltage6V(int32_t *status){ 
 		return 0.0f; 
 	}
-	float getUserCurrent6V(int32_t *status) 
-	{ 
+	float getUserCurrent6V(int32_t *status) { 
 		return 0.0f; 
 	}
-	bool getUserActive6V(int32_t *status) 
-	{ 
+	bool getUserActive6V(int32_t *status){ 
 		return true; 
 	}
-	int getUserCurrentFaults6V(int32_t *status) 
-	{ 
+	int getUserCurrentFaults6V(int32_t *status){ 
 		return 0; 
 	}
-	float getUserVoltage5V(int32_t *status)
-	{ 
+	float getUserVoltage5V(int32_t *status){ 
 		return 0.0f; 
 	}
-	float getUserCurrent5V(int32_t *status)
-	{ 
+	float getUserCurrent5V(int32_t *status){ 
 		return 0.0f; 
 	}
-	bool getUserActive5V(int32_t *status) 
-	{ 
+	bool getUserActive5V(int32_t *status){ 
 		return false; 
 	}
-	int getUserCurrentFaults5V(int32_t *status) 
-	{ 
+	int getUserCurrentFaults5V(int32_t *status){ 
 		return 0; 
 	}
-	float getUserVoltage3V3(int32_t *status)
-	{ 
+	float getUserVoltage3V3(int32_t *status){ 
 		return 0.0f; 
 	}
-	float getUserCurrent3V3(int32_t *status)
-	{ 
+	float getUserCurrent3V3(int32_t *status){ 
 		return 0.0f; 
 	}
-	bool getUserActive3V3(int32_t *status) 
-	{ 
+	bool getUserActive3V3(int32_t *status) { 
 		return false; 
 	}
-	int getUserCurrentFaults3V3(int32_t *status) 
-	{ 
+	int getUserCurrentFaults3V3(int32_t *status) { 
 		return 0; 
 	}
 }
